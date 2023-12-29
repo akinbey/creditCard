557e53a441334e2a89e8adf245149c89887360a6
Emre Akıncı <frm.red22@gmail.com>
2023-12-29 17:50:07 +0300

    first

---
+++ src/models/user.ts
@@ -0,0 +1,52 @@
+import { Card } from "./card";
+
+export interface User{
+    id:number;
+    name:string;
+    surname:string;
+    age:number;
+}
+
+/*
+{
+    "id": 1,
+    "name": "John",
+    "surname": "Doe",
+    "age": 30
+  },
+*/
+
+export interface UserWithCardDto extends User{
+  cards:Card[]
+}
+
+/*
+[
+  {
+    "id": 1,
+    "name": "John",
+    "surname": "Doe",
+    "age": 30,
+    "cards": [
+      {
+        "id": 62,
+        "userId": 1,
+        "number": "5602242626999444",
+        "type": "bankcard"
+      },
+      {
+        "id": 83,
+        "userId": 1,
+        "number": "6399283404521468",
+        "type": "instapayment"
+      },
+      {
+        "id": 95,
+        "userId": 1,
+        "number": "4911485813784793",
+        "type": "switch"
+      }
+    ]
+  }
+]
+*/
